df37cfdb9a47fde84e7b8671d4b3ba6083be6845
Wojciech <wojciech.gladalski@gmail.com>
2019-10-30 20:08:43 +0100

    updated bookdao

--- src/main/java/pl/coderslab/repository/BookDao.java
+++ src/main/java/pl/coderslab/repository/BookDao.java
@@ -17,6 +17,10 @@ public class BookDao {
     public BookDao() {
         this.list = new ArrayList<>();
         list.add(new Book(1L,"9788324631766","Thinking in Java", "Bruce Eckel","Helion","programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz glowa, Java.",
+                "Sierra Kathy, Bates Bert", "Helion", "programming"));
+        list.add(new Book(3L, "9780130819338", "Java 2. Podstawy",
+                "Cay Horstmann, Gary Cornell", "Helion", "programming"));
     }
 
 }
